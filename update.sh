61526eb3bb9f6cf5d420f720fd3561ca093cf652
Brendon Mendicino <brendonmedicino@yahoo.it>
2022-11-19 17:17:38 +0100

    sab 19 nov 2022, 17:17:38, CET

--- update.sh
+++ update.sh
@@ -4,8 +4,20 @@ SCRIPT_PATH="$( dirname -- "$( readlink -f -- "$0"; )"; )"
 
 cd $SCRIPT_PATH
 
+for file in $(ls -C); do
+    ! [ -d "$file" ] && continue
+    ! [ $(ls $file | grep -E "^.*\.tex$") ] && continue
+
+    cd $file
+    echo $(pwd)
+    echo "Converting $(ls | grep -E "^.*\.tex$") ..."
+    pdflatex "$(ls | grep -E "^.*\.tex$")"
+    cd ..
+done
 
 
+echo "Adding to git..."
 git add *
+echo "Performing commit..."
 git commit -m "$(date)"
 git push --all
