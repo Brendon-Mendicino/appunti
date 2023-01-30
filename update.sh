cddbd1ac548eaee1159d18203227d5c4de7d8e47
Brendon Mendicino <brendonmendicino@yahoo.it>
2023-01-30 12:15:59 +0100

    Mon Jan 30 12:15:59 PM CET 2023

--- update.sh
+++ update.sh
@@ -2,23 +2,24 @@
 
 SCRIPT_PATH="$( dirname -- "$( readlink -f -- "$0"; )"; )"
 
-cd $SCRIPT_PATH
+cd "$SCRIPT_PATH" || exit
 
-for file in $(ls -C); do
+for file in ./* ; do
     ! [ -d "$file" ] && continue
-    ! [ $(ls $file | grep -E "^.*\.tex$") ] && continue
+    ! [ $(ls "$file" | grep -E "^.*\.tex$") ] && continue
 
-    cd $file
+    cd "$file" || continue
 
     # if the file contains not tex file changed go to the next one
     ! [ $(git ls-files -m | grep -E "^.*\.tex$") ] && cd .. && continue
 
     echo $(pwd)
     echo "Converting $(ls | grep -E "^.*\.tex$") ..."
-    pdflatex "$(ls | grep -E "^.*\.tex$")"
+    pdflatex "$(ls | grep -E "^.*\.tex$")" &
     cd ..
 done
 
+wait
 
 echo "Adding to git..."
 git add *
